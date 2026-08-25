b7218d441d99e5d88a940fa5a9e1e36df2b053c8
jp <que.johnn0@gmail.com>
2026-08-25 21:26:30 +0800

    timer's blinking 1000ms test works

--- src/main.c
+++ src/main.c
@@ -2,7 +2,34 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#include <tmr1.h>
+
+volatile uint64_t nowTime = 0;
+volatile uint64_t prevTime = 0;
+
 int main(void){
+  
+  DDRB |= (1 << 5);
+  PORTB = 0x00;
+
+  timer1_init();
+
+  
+  while(1){
+  }
+    
+
+  
 
   return 0;
+}
+
+void testTimer(void){
+  nowTime = getTime();
+  if(((uint64_t) nowTime - prevTime) >= 1000){
+    PORTB ^= (1 << 5);
+    prevTime = nowTime;
+
+  }
+  return;
 }
