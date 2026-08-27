99917ae3a3e57dd17d3ba0fc3218537fc3130347
jp <que.johnn0@gmail.com>
2026-08-27 14:30:02 +0800

    test usart transmit

--- src/main.c
+++ src/main.c
@@ -4,30 +4,35 @@
 
 #include <tmr1.h>
 #include <adc.h>
+#include <usart0.h> 
 
 volatile uint64_t nowTime = 0;
 volatile uint64_t prevTime = 0;
 
 volatile uint16_t frame[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-
+/*
 ISR(TIMER1_COMPA_vect){
   adc_read(frame);
 
   return;
 }
+*/
 
 int main(void){
   
   DDRB |= (1 << 5);
   PORTB = 0x00;
 
-  timer1_init();
-  adc_init();
+  //timer1_init();
+  //adc_init();
+  usart0_init();
 
   
   while(1){
-
+    for(uint8_t i = 0; i < 0xFF; i++){
+      usart0_transmit(i);
+    }
   }
     
 
