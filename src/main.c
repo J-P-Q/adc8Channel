bf8aabcc66576905a8042d01a46b88e703878bae
jp <que.johnn0@gmail.com>
2026-08-27 14:18:38 +0800

    Setup frame and ISR to adcRead(), but not tested)

--- src/main.c
+++ src/main.c
@@ -3,9 +3,19 @@
 #include <avr/interrupt.h>
 
 #include <tmr1.h>
+#include <adc.h>
 
 volatile uint64_t nowTime = 0;
 volatile uint64_t prevTime = 0;
+
+volatile uint16_t frame[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+
+ISR(TIMER1_COMPA_vect){
+  adc_read(frame);
+
+  return;
+}
 
 int main(void){
   
@@ -13,9 +23,11 @@ int main(void){
   PORTB = 0x00;
 
   timer1_init();
+  adc_init();
 
   
   while(1){
+
   }
     
 
